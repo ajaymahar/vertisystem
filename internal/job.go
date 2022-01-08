3d19ec38292ab4353cb133bf1bbad80343782a44
Ajay Kumar Mahar <ajay.mhar@gmail.com>
2022-01-08 11:13:29 +0530

    stub impl cahanges

--- internal/job.go
+++ internal/job.go
@@ -1,7 +1,12 @@
 package internal
 
-//
+// Job is type struct will define req text
 type Job struct {
 	ID   string
 	Text string
+}
+
+type JobResult struct {
+	ID        string
+	Frequency map[string]int // word with it's occurance
 }
--- internal/repository/jobstub.go
+++ internal/repository/jobstub.go
@@ -11,25 +11,25 @@ import (
 // StubRepository implementation for the JobRepo
 type StubRepository struct {
 	// temp in memory data store
-	jobs map[string]string
+	// jobs map[string]string
+	jobs map[internal.Job]internal.JobResult
 }
 
 // NewStubRepository is factory function to create new
 func NewStubRepository() *StubRepository {
 	return &StubRepository{
-		jobs: make(map[string]string),
+		jobs: make(map[internal.Job]internal.JobResult),
 	}
 }
 
 func (sr *StubRepository) Create(ctx context.Context, text string) (internal.Job, error) {
-	id := getNewID()
-	fmt.Println("id", id)
-	sr.jobs[id] = text
-	fmt.Printf("%#v", sr.jobs)
-	return internal.Job{
-		ID:   id,
+	j := internal.Job{
+		ID:   getNewID(),
 		Text: text,
-	}, nil
+	}
+	sr.jobs[j] = internal.JobResult{}
+	fmt.Printf("%+v", sr.jobs)
+	return j, nil
 }
 
 //Generate new ID for each job
